bf60359eabb188ddce5cb6e97a11ef633fe309db
LUC TUNG <tungltph25765@fpt.edu.vn>
2024-06-03 20:05:50 +0700

    them user

---
+++ app/src/main/java/com/example/datn_md16/activity_user_profile.java
@@ -0,0 +1,14 @@
+package com.example.datn_md16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_user_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
