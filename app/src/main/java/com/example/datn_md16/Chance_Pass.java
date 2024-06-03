2900ada0030eabc6cccbd2b223aa09c2b54655a4
LUC TUNG <tungltph25765@fpt.edu.vn>
2024-06-03 21:52:18 +0700

    them user

---
+++ app/src/main/java/com/example/datn_md16/Chance_Pass.java
@@ -0,0 +1,14 @@
+package com.example.datn_md16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chance_Pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chance_pass);
+    }
+}
