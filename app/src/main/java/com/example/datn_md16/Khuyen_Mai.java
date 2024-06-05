90219493396d1df437f527d2e3bacc1c81dd922d
LUC TUNG <tungltph25765@fpt.edu.vn>
2024-06-05 17:42:37 +0700

    them khuyen mai

---
+++ app/src/main/java/com/example/datn_md16/Khuyen_Mai.java
@@ -0,0 +1,14 @@
+package com.example.datn_md16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Khuyen_Mai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_khuyen_mai);
+    }
+}
